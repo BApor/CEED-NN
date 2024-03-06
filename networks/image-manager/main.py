e1a6312cbab6fb1d71fa4976ce456c5cdd65f8e2
BApor <bapor2002@gmail.com>
2024-03-06 08:15:35 +0200

    Started to write image-manager

--- networks/image-manager/main.py
+++ networks/image-manager/main.py
@@ -1,10 +1,13 @@
 import cv2 as cv
 
-image = cv.imread("../datasets/primitive/3. Field Pea/images/IMG_4148.JPG")
+image = cv.imread("../images-labels/primitive/3. Field Pea/images/IMG_4148.JPG")
 height, width, _ = image.shape
-print(str(width) + "x" + str(height))
+print("Dimensions: " + str(width) + "x" + str(height))
 
+xtl = 1670
+ytl = 424
+xbr = 1881
+ybr = 601
 
-
-cut = image[0:1000, 0:1000]
+cut = image[424:601, 1670:1881]
 cv.imwrite("cut.jpg", cut)
