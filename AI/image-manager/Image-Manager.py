8c0ff14f4c6b8531b17f99a07aee2467669ec0b1
BApor <bapor2002@gmail.com>
2024-03-07 16:12:56 +0200

    Adding Error Handling to Image Manager

--- AI/image-manager/Image-Manager.py
+++ AI/image-manager/Image-Manager.py
@@ -13,34 +13,41 @@ def process_images_of_type(seed_type: str, path: str, dataset_path: str, to_size
 
     # Checking if the directories are correctly given
 
+    red_text = "\033[91m"
+    reset_text = "\033[0m"
+
     try:
-        if not os.path.exists(path) and os.path.isdir(path):
-            print("Image Manager Error: The given IMAGE AND LABELS path does not exists or is not a directory!")
+        if not os.path.exists(path) or not os.path.isdir(path):
+            print(f"{red_text}Image Manager Error: The given work path does not exists or is not a directory!\n")
+            print(f"{path}{reset_text}")
             return
     except OSError as e:
         print(f"Image Manager Error: {e}")
         return
 
-    type_path = f"{path}/{seed_type}"
+    # The path to the seed images
+    seed_type_path = f"{path}/{seed_type}"
 
     try:
-        if not os.path.exists(f"{type_path}/images") and os.path.isdir(f"{type_path}/images") \
-        or not os.path.exists(f"{type_path}/labels") and os.path.isdir(f"{type_path}/labels"):
-            print("Image Manager Error: The IMAGE AND LABELS isn't following the next file architecture:")
+        if not os.path.exists(f"{seed_type_path}/images") or not os.path.isdir(f"{seed_type_path}/images") \
+        or not os.path.exists(f"{seed_type_path}/labels") or not os.path.isdir(f"{seed_type_path}/labels"):
+            print(f"{red_text}Image Manager Error: The given work path isn't following the next file architecture:")
             print("\t\t:")
             print("\t\t:")
             print("\t\t| image_and_labels_path")
             print("\t\t       | seed_type")
             print("\t\t             | images")
-            print("\t\t             | labels\n")
+            print(f"\t\t             | labels\n{reset_text}")
             return
     except OSError as e:
         print(f"Image Manager Error: {e}")
         return
 
     try:
-        if not os.path.exists(dataset_path) and os.path.isdir(dataset_path):
-            print("Error: The given DATASET DESTINATION PATH does not exists or is not a directory!")
+        if not os.path.exists(dataset_path) or not os.path.isdir(dataset_path):
+            print(f"{red_text}Image Manager Error: The given dataset destination path "
+                  "does not exists or is not a directory!")
+            print(f"{dataset_path}{reset_text}")
             return
     except OSError as e:
         print(f"Image Manager Error: {e}")
@@ -51,17 +58,18 @@ def process_images_of_type(seed_type: str, path: str, dataset_path: str, to_size
     xml_file = None
 
     try:
-        with open(f"{type_path}/labels/annotations.xml", "r") as file:
+        with open(f"{seed_type_path}/labels/annotations.xml", "r") as file:
             xml_file = file.read()
     except FileNotFoundError:
-        print("Image Manager Error: The labels file annotations.xml in the given labels directory does not exist!")
+        print(f"{red_text}Image Manager Error: The labels file annotations.xml in "
+              f"the given labels directory does not exist!{reset_text}")
         return
     except IOError as e:
         print(f"Image Manager Error: Unable to read the labels file annotations.xml! {e}")
         return
 
     if xml_file is None:
-        print("Image Manager Error: The labels file annotations.xml is empty!")
+        print(f"{red_text}Image Manager Error: The labels file annotations.xml is empty!{reset_text}")
         return
 
     # Processing images
@@ -77,7 +85,7 @@ def process_images_of_type(seed_type: str, path: str, dataset_path: str, to_size
         # else if it is a box then process image and export the labeled
         if row.strip().lower().startswith('<image'):
             image_name = row.split()[2].split('"')[1]
-            image = cv.imread(f"{type_path}/images/{image_name}")
+            image = cv.imread(f"{seed_type_path}/images/{image_name}")
             continue
         elif row.strip().lower().startswith('<box'):
             row_array = row.split('"')
@@ -106,7 +114,34 @@ def process_images_of_type(seed_type: str, path: str, dataset_path: str, to_size
             image_from_label_id += 1
 
 
-process_images_of_type("3. Austrian Winter Pea",
-                       "/Volumes/APORKA SSD/Allamvizsga/Program/CEED-NN/AI/plantseeds/",
-                       "/Volumes/APORKA SSD/Allamvizsga/Program/CEED-NN/AI/image-manager/datasets",
-                       (0, 0))
+# process_images_of_type("3. Austrian Winter Pea",
+#                        "/Volumes/APORKA SSD/Allamvizsga/Program/CEED-NN/AI/image-manager/datasets",
+#                        "/Volumes/APORKA SSD/Allamvizsga/Program/CEED-NN/AI/image-manager/datasets",
+#                        (0, 0))
+
+def process_images_all(path: str, dataset_path: str):
+    # Checking if the directories are correctly given
+
+    red_text = "\033[91m"
+    reset_text = "\033[0m"
+
+    try:
+        if not os.path.exists(path) or not os.path.isdir(path):
+            print(f"{red_text}Image Manager Error: The given work path does not exists or is not a directory!")
+            print(f"{path}{reset_text}")
+            return
+    except OSError as e:
+        print(f"Image Manager Error: {e}")
+        return
+
+    try:
+        if not os.path.exists(dataset_path) or not os.path.isdir(dataset_path):
+            print(f"{red_text}Image Manager Error: The given dataset destination path "
+                  "does not exists or is not a directory!")
+            print(f"{dataset_path}{reset_text}")
+            return
+    except OSError as e:
+        print(f"Image Manager Error: {e}")
+        return
+
+process_images_all("/Volumes/APORKA SSD/Allamvizsga/Program/CEED-NN/AI/image-manager/datasets", "/adas/ad")
